775cfd110b351991bc89723a381c86bac5b65ffe
swo <swo@alum.mit.edu>
2019-12-05 18:16:28 -0500

    uclust with "pick_closed_reference_otus.py"

--- qiime1/do-uclust.py
+++ qiime1/do-uclust.py
@@ -9,13 +9,18 @@ def run_uclust(prefix, force=False):
         return None
 
     with tempfile.TemporaryDirectory() as tmp_dir:
-        # -z enables reverse strand
-        # -C suppresses new clusters (i.e., does reference-based)
-        # -m selects the method
-        command = ["pick_otus.py", "-z", "-C", "-m", "uclust_ref", "-i", "../{}-deblur-seqs.fasta".format(prefix), "-o", tmp_dir]
+        # -f overwrites directories
+        # -p points to the params file, which has an option for reverse strand
+        # searching (for the reverse sequences)
+        command = ["pick_closed_reference_otus.py", "-f", "-p", "params.config", "-i", "../{}-deblur-seqs.fasta".format(prefix), "-o", tmp_dir]
+
+        # An alternative is to use pick_otus.py with -z (reverse strand search), -C (suppress new clusters, i.e., do reference-based search),
+        # and -m uclust_ref (i.e., use the default closed reference method):
+        # command = ["pick_otus.py", "-z", "-C", "-m", "uclust_ref", "-i", "../{}-deblur-seqs.fasta".format(prefix), "-o", tmp_dir]
+
         subprocess.call(command)
 
-        src = os.path.join(tmp_dir, "{}-deblur-seqs_otus.txt".format(prefix))
+        src = os.path.join(tmp_dir, "uclust_ref_picked_otus", "{}-deblur-seqs_otus.txt".format(prefix))
         command = ["mv", src, dest]
         subprocess.call(command)
 
